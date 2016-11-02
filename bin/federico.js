327809717e883bc733640c577f15798537f0d936
Rocco Janse <rocco.janse@efocus.nl>
2016-11-02 22:57:11 +0100

    Updated script

--- bin/federico.js
+++ bin/federico.js
@@ -1,2 +1,11 @@
 #! /usr/bin/env node
-console.log("federico: output");
+
+var command = process.argv[2] || null,
+    filename = process.argv[3] || '';
+
+console.log(command + ': ' + filename);
+
+if (command === null) {
+    console.log('Usage: federico <command> [filename]');
+    process.exit(1);
+}
