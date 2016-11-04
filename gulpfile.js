c91afacafcf7661ef088f71b6ce01082bcc36d57
Rocco Janse <rocco.janse@efocus.nl>
2016-11-04 12:32:23 +0100

    added docs

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@
 var gulp = require('gulp'),
     jsdoc = require('gulp-jsdoc3');
 
+'use strict';
+
 gulp.task('docs', function(cb) {
     var config = require('./jsdoc.json');
-    gulp.src(['./README.md', './index.js', './Frontend/**/*.js'], {
+    gulp.src(['./README.md', './index.js', './src/**/*.js'], {
         read: false
     })
     .pipe(jsdoc(cb));
--- index.js
+++ index.js
@@ -3,30 +3,19 @@
 'use strict';
 
 var version = require('./package').version,
-    program = require('commander');
+    program = require('commander'),
+    files = require('./src/files');
 
-/**
- * creates files
- * @param {string} type Type of object to create (component/element)
- * @param {string} name Name of new object
- * @param {object} options Options defined in cli program call
- * @param {boolean} options.html True if defined in cli
- * @param {boolean} options.js True if defined in cli
- * @param {boolean} options.sass True if defined in cli
- */
-function create(type, name, options) {
-    console.log('create %s "%s"', type, name, options.html, options.js, options.sass);
-}
 
 program.version(version);
 
 program
     .command('create <type> <name>')
     .description('create a component or element')
-    .option("--html", "Only create html")
-    .option("--scss, --sass", "Only create sass")
-    .option("--js", "Only create javascript")
-    .action(create)
+    .option("--html", "Skips html file creation")
+    .option("--scss", "Skips sass file creation")
+    .option("--js", "Skips javascript file creation")
+    .action(files.createFiles)
     .on('--help', function () {
         console.log('  Examples:');
         console.log();
